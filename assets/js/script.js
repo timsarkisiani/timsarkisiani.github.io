8792a963f7cbc4bf7c9242fbc3aaf4b062b60307
Tim Sarkisiani <timsarkisiani@gmail.com>
2022-11-15 00:23:53 +0400

    111222

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,6 +120,8 @@ const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
+
+
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
